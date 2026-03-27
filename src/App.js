bfeee54f37b3552eff39bfa78d30c30dc5b0074a
Cindy <swi963@gmail.com>
2026-03-27 12:56:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import React, { useState, useEffect } from 'react';
 import { Mail, ChevronRight, Menu, X, Cpu, Home, ShieldCheck, Wifi, ExternalLink, Users, Layout, Smartphone, ArrowRight, Palette, Globe, Eye } from 'lucide-react';
 
